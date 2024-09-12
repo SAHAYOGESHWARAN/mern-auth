9c8456a82129cdee2302c5bcfba4362474d279ba
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-12 22:23:58 +0530

    ma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Login from './components/Login';
 import Register from './components/Register';
 import EmailVerification from './components/EmailVerification';
 import PasswordRecovery from './components/PasswordRecovery';
+import ResetPassword from './components/ResetPassword';
+import Dashboard from './components/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
@@ -13,6 +16,8 @@ const App = () => {
         <Route path="/register" component={Register} />
         <Route path="/verify-email/:token" component={EmailVerification} />
         <Route path="/password-recovery" component={PasswordRecovery} />
+        <Route path="/reset-password/:token" component={ResetPassword} />
+        <ProtectedRoute path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );
