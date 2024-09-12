d52508a43f11e9f9533cb1037d9e5986844717bc
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-12 22:19:12 +0530

    ma

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, verifyEmail, login, passwordRecovery } = require('../controllers/authController');
+const { register, login, verifyEmail, passwordRecovery, resetPassword } = require('../controllers/authController');
 const router = express.Router();
 
+// Routes
 router.post('/register', register);
-router.post('/verify-email', verifyEmail);
 router.post('/login', login);
+router.post('/verify-email', verifyEmail);
 router.post('/password-recovery', passwordRecovery);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
