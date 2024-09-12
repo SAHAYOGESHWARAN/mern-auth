3e2eb33dec83db845e23108b4aa71896a95b57d3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-12 21:56:08 +0530

    MA

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+const User = require('../models/User');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
+
+// Register user, send verification email logic here
+
+// Password recovery logic here
