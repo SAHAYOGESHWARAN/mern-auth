49cc4a82988d2d85793d05d7dfa4f4f1071a7776
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-12 22:21:11 +0530

    ma

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -25,3 +25,30 @@ const sendEmail = async (to, subject, text) => {
 };
 
 module.exports = sendEmail;
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (to, subject, text) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to,
+      subject,
+      text,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.log('Error sending email', error);
+  }
+};
+
+module.exports = sendEmail;
